faa3af3f18d58fd0339f44e178b371afba941f10
abderrahman-bouamama <bouamama.abderrahmane95@gmail.com>
2025-11-21 23:18:46 +0100

    Delete digital-solutions-app

--- digital-solutions-app/storage/framework/views/07189941cbff1c8d468380e371070d12.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\lenovo\Documents\GitHub\pfe-web-app-management\pfe-web-app-management\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
--- digital-solutions-app/storage/framework/views/e0985b533df560013b11adf5ec3a4741.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\Users\lenovo\Documents\GitHub\pfe-web-app-management\pfe-web-app-management\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
